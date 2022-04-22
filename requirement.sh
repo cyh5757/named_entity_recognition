1f1da94146a9fdb8adc76f1c37f468bd2f00a648
Meto <cyh8781@gmail.com>
2022-04-22 10:27:42 +0000

    seqeval requirement

--- requirement.sh
+++ requirement.sh
@@ -2,3 +2,4 @@ pip install transformers
 # pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
 pip install 'git+https://github.com/monologg/KoBERT-Transformers.git#egg=kobert_transformers'
 pip install datasets
+pip install seqeval
