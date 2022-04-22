8d5e69886237025a7b01221aaf9fcf407039b0d7
Meto <cyh8781@gmail.com>
2022-04-22 07:58:48 +0000

    kobert-transformers error modify

--- requirement.sh
+++ requirement.sh
@@ -1,4 +1,4 @@
 pip install transformers
 pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
-pip install 'git+https://github.com/monologg/KoBERT-Transformers.git'
+pip3 install kobert-transformers
 pip install datasets
